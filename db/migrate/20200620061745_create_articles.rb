bfedc5434c062641cf18d19d18d7b9ee46e8c95a
masataka <hanayoriinko@gmail.com>
2020-06-20 16:01:09 +0900

    add gem devise

---
+++ db/migrate/20200620061745_create_articles.rb
@@ -0,0 +1,14 @@
+class CreateArticles < ActiveRecord::Migration[5.2]
+  def change
+    create_table :articles do |t|
+      t.integer :category_id
+      t.integer :comment_id
+      t.integer :like_id
+      t.string :title
+      t.text :body
+      t.string :image_id
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_20_042159) do
+ActiveRecord::Schema.define(version: 2020_06_20_061745) do
+
+  create_table "articles", force: :cascade do |t|
+    t.integer "category_id"
+    t.integer "comment_id"
+    t.integer "like_id"
+    t.string "title"
+    t.text "body"
+    t.string "image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
