770efa51308f16991d6041ee7a8ff32bd167b065
masataka michikawa <masataaaka3@icloud.com>
2020-07-13 10:02:26 +0000

    fix-indent

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -63,4 +63,11 @@ $('[data-provider="summernote"]').each(function(){
   });
 });
 
-
+$(function() {
+  $('#back a').on('click',function(event){
+    $('body, html').animate({
+      scrollTop:0
+    }, 800);
+    event.preventDefault();
+  });
+});
